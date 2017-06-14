9a35045b311799764b391f0d5d58adf06c06fc5a
Mohamed Warsame <mwarsame9@gmail.com>
2017-06-14 16:22:13 -0700

    deploy with firebase

---
+++ src/app/api-keys.ts
@@ -0,0 +1,7 @@
+export var masterFirebaseConfig = {
+    apiKey: "AIzaSyCAAZU66JybQzjcUTAyPXo-wb8-5IAfQWk",
+    authDomain: "online-store-a93aa.firebaseapp.com",
+    databaseURL: "https://online-store-a93aa.firebaseio.com",
+    storageBucket: "online-store-a93aa.appspot.com",
+    messagingSenderId: "1021148138942"
+  };
